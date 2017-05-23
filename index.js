8bcb76c954b57b5ee48a2cd620bc518603198a16
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-05-24 00:22:46 +0200

    Lays out basis for propagating the request id on service calls.

--- index.js
+++ index.js
@@ -8,15 +8,25 @@ function microbrew(module, deps = {}, tracer, debug) {
     const app = express();
 
     const { middleware: rpcMiddleware, consumeWith } = rpc(module, {debug});
-    const run = consumeWith(got);
+    const serviceCall = (host, method, args, requestData) => {
+        // TODO here is where we can control the retry-on-error logic, after the consumer API is defined
+        // TODO we need to configure the requester so that it will propagate the request data via headers
+        const run = consumeWith(got);
+
+        return run(host, method, args);
+    };
 
     const { middleware: wakeMiddleware, error: wakeError, decorator: traceFn } = wake(tracer);
 
     const doIO = io => typeof io === 'function' ? io() : io;
     const mapDep = service => service in deps ? deps[service] : service;
     const camelKebap = camel => camel.replace(/([a-z])([A-Z0-9])|([0-9])([a-zA-Z])/g, (_, l, u, ll, uu) => `${l||ll}-${u||uu}`).toLowerCase();
-    const proxy = (service, method, ...args) => service === 'io' ? doIO(method) : run(mapDep(camelKebap(service)), method, args);
     const configTrace = (service, method) => service === 'io' ? { service } : { service, method };
+
+    // we need a real function to have a this argument
+    function proxy(service, method, ...args) {
+        return service === 'io' ? doIO(method) : serviceCall(mapDep(camelKebap(service)), method, args, this);
+    }
 
     local(traceFn('proxy', configTrace, proxy));
 
