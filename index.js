c69cc671d8328a58a2de7361cb4b72e5eaa77e70
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2017-05-24 20:13:18 +0200

    Allows configuring hostnames and ports as well.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { PKG_NAME, PKG_VERSION } = require('./package.json');
+const DEFAULT_PORT = 5000;
 
 const express = require('express');
 const wake = require('express-wake');
@@ -6,11 +7,11 @@ const rpc = require('express-rpc-beeson');
 const local = require('local-rpc');
 const got = require('got');
 
-function microbrew(name, module, deps = {}, tracer, debug, gateway) {
+function microbrew(name, module, aliases = {}, tracer, debug, gateway) {
     const app = express();
 
-    const { middleware: rpcMiddleware, consumeWith } = rpc(module, {debug});
-    const serviceCall = (host, method, args, requestData) => {
+    const { middleware: rpcMiddleware, consumeWith } = rpc(module, { debug });
+    const serviceCall = (service, method, args, requestData) => {
         const run = consumeWith((url, options) => {
             options.headers = {
                 'user-agent': `${name} (${PKG_NAME}/${PKG_VERSION})`,
@@ -22,20 +23,22 @@ function microbrew(name, module, deps = {}, tracer, debug, gateway) {
 
             return got(url, options);
         });
+        const buildConfig = aliasConfig => typeof aliasConfig === 'string' ? { host: aliasConfig } : aliasConfig;
+        const resolve = service => service in aliases ? buildConfig(aliases[service]) : { host: service };
+        const { host, port } = Object.assign({ port: DEFAULT_PORT }, resolve(service));
 
-        return run(host, method, args, {port: 5000});
+        return run(host, method, args, { port });
     };
 
     const { middleware: wakeMiddleware, error: wakeError, decorator: traceFn } = wake(tracer);
 
     const doIO = io => typeof io === 'function' ? io() : io;
-    const mapDep = service => service in deps ? deps[service] : service;
     const camelKebap = camel => camel.replace(/([a-z])([A-Z0-9])|([0-9])([a-zA-Z])/g, (_, l, u, ll, uu) => `${l||ll}-${u||uu}`).toLowerCase();
     const configTrace = (service, method) => service === 'io' ? { service } : { service, method };
 
     // we need a real function to have a this argument
     function proxy(service, method, ...args) {
-        return service === 'io' ? doIO(method) : serviceCall(mapDep(camelKebap(service)), method, args, this);
+        return service === 'io' ? doIO(method) : serviceCall(camelKebap(service), method, args, this);
     }
 
     local(traceFn('proxy', configTrace, proxy));
@@ -45,15 +48,15 @@ function microbrew(name, module, deps = {}, tracer, debug, gateway) {
     if (tracer) app.use(wakeError);
 
     if (gateway) return app;
-    else return function start(port = 5000) {
+    else return function start(port = DEFAULT_PORT) {
         const server = app.listen(port);
 
         return { server, app };
     };
 }
 
-microbrew.debug = (name, module, deps, tracer) => microbrew(name, module, deps, tracer, true);
+microbrew.debug = (name, module, aliases, tracer) => microbrew(name, module, aliases, tracer, true);
 // debug is only relevant for the rpcMiddleware
-microbrew.gateway = (name, module, deps, tracer) => microbrew(name, module, deps, tracer, false, true);
+microbrew.gateway = (name, module, aliases, tracer) => microbrew(name, module, aliases, tracer, false, true);
 
 module.exports = microbrew;
